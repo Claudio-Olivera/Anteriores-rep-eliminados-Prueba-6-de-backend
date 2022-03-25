354d0b7f205ad26fabe8e18ffc3a85abaffdbab0
Claudio-Olivera <96873549+Claudio-Olivera@users.noreply.github.com>
2022-03-25 16:23:10 -0300

    Add files via upload

---
+++ src/main/java/claudioServer/ClaudioServer.java
@@ -0,0 +1,14 @@
+package claudioServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ClaudioServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClaudioServer.class, args);
+    }
+
+}
---
+++ src/main/java/claudioServer/controller/ExperienciaController.java
@@ -0,0 +1,35 @@
+package claudioServer.controller;
+
+
+import claudioServer.model.Experiencia;
+import claudioServer.service.IExperienciaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+
+public class ExperienciaController {
+
+    @Autowired
+    private IExperienciaService experienciaService;
+
+    @PostMapping("/new/experiencia")
+    public void agregarExperiencia (@RequestBody Experiencia exp) {
+        experienciaService.crearExperiencia(exp);
+    }
+
+    @GetMapping("/ver/experiencia")
+    @ResponseBody
+    public List<Experiencia> verExperiencia () {
+        return experienciaService.verExperiencia();
+    }
+
+    @DeleteMapping ("/borrarExperiencia/{id}")
+    public void borrarExperiencia(@PathVariable Long id){
+        experienciaService.borrarExperiencia(id);
+    }
+
+
+}
